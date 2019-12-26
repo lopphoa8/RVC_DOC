e3b1ee13195d28d4fd6687c2aa87a088b9521ce3
lopphoa8 <44536007+lopphoa8@users.noreply.github.com>
2019-12-26 19:09:52 +0700

    Add files via upload

--- print3.py
+++ print3.py
@@ -1,13 +1,22 @@
 # -*- coding: utf-8 -*-
 import openpyxl
+import time
 
-file1 = 'MarketingAnalystNames.xlsx'
-file2 = 'MarketingAnalystNames_Merge.xlsx'
+file1 = 'MarketingAnalystNames.xlsm'
+file2 = 'MarketingAnalystNames_Merge.xlsm'
+file3 = 'MarketingAnalystNames_Merge.xlsm'
 
-wb1 = openpyxl.load_workbook(filename=file1)
-wb2 = openpyxl.load_workbook(filename=file2)
+start_time = time.time()
 
-print(len(wb1.sheetnames))
+#wb1 = openpyxl.load_workbook(filename=file1, read_only=False, keep_vba=True)
+#wb2 = openpyxl.load_workbook(filename=file2, read_only=False, keep_vba=True)
+
+wb1 = openpyxl.load_workbook(filename=file1, read_only=True, keep_vba=False)
+wb2 = openpyxl.load_workbook(filename=file2, keep_vba=True)
+
+print("--- %s seconds ---" % (time.time() - start_time))
+
+#print(len(wb1.sheetnames))
 sheets = wb1.sheetnames
 
 for sh in range (len(wb1.sheetnames)):
@@ -15,12 +24,18 @@ for sh in range (len(wb1.sheetnames)):
         sheet2 = wb2.get_sheet_by_name(sheets[sh])
         for i in range(1,sheet1.max_row+1):
                 for j in range(1,sheet1.max_column+1):
-                                print(sh,i,j,sheet1.cell(i,j).value)
+                                #print(sh,i,j,sheet1.cell(i,j).value)
                                 sheet2.cell(i,j).value = sheet1.cell(i,j).value
-                                print(sh,i,j,sheet2.cell(i,j).value)
+                                #print(sh,i,j,sheet2.cell(i,j).value)
 
                                 
 
 # Escribirmos en el Fichero
-wb1.save(file1)
-wb2.save(file2)
+
+print("--- %s seconds ---" % (time.time() - start_time))
+
+#wb1.save(file1)
+#wb2.save(file2)
+wb2.save(file3)
+
+print("--- %s seconds ---" % (time.time() - start_time))
